798d2a3a0183e3192ad6e4d4921468060fa3befd
samvarankashyap <samvaran.kashyap@gmail.com>
2017-03-04 14:44:13 -0500

    updated names of files and added some ui improvements

--- index.py
+++ index.py
@@ -5,10 +5,9 @@ from flask import Flask, request, send_from_directory
 
 app = Flask(__name__,static_url_path='/static')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/getmoduleschema/<name>')
@@ -53,33 +52,6 @@ def send_css(path):
 @app.route('/deps/<path:path>')
 def send_deps(path):
     return send_from_directory('./static/deps', path)
-
-
-@app.route('/')
-def index():
-    return 'Index Page'
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
 
 
 if __name__ == '__main__':
