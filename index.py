1e29d27aaf9460cccc3bd14c2d0f84293f2c5374
samvaran <samvaran2best@gmail.com>
2017-06-18 22:22:07 +0000

    added playbooks updated api for get task list and tasklist output

--- index.py
+++ index.py
@@ -13,18 +13,27 @@ def find(name, path):
             if name == file.strip(".json"):
                 return path+dir+"/"+file
     
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/getmoduleschema/<name>')
 def get_module_schema(name):
     file_path =  find(name, "./schemas/")
     schema = open(file_path, "r").read()
     return schema
+
+@app.route('/gettasklist/')
+def get_task_list():
+    task_list = os.listdir("/root/.ansible_async/")
+    return json.dumps(task_list)
+
+@app.route('/gettaskoutput/<taskid>')
+def get_task_output(taskid):
+    task_file = "/root/.ansible_async/"+taskid
+    import pdb
+    pdb.set_trace()
+    return open(task_file, "r").read()
 
 @app.route('/listmodules/')
 def list_modules():
@@ -36,22 +45,18 @@ def list_modules():
         modules_list[dir] = schema_files
     return json.dumps(modules_list)
 
-
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('./static/js', path)
-
 
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('./static/css', path)
 
-
 @app.route('/deps/<path:path>')
 def send_deps(path):
     return send_from_directory('./static/deps', path)
 
-
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
